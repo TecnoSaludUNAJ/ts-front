9fefde284435f6639f363ce3dd3960109c457e13
julinuzzo19 <julinuzzo19@gmail.com>
2020-10-20 01:28:57 -0300

    /Profesional template bugs fixed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const routes = require('express').Router();
 const usuario = require('./usuario')
+const profesional= require('./profesional')
 
 //index
 routes.get('/', (req, res) => {res.render('index', {title: 'Inicio | Tecnosalud',})})
 
 // routes de usuario
 routes.use('/usuario', usuario)
+routes.use('/profesional',profesional)
 
 //404
 routes.use(function(req, res, next) {res.status(404).render('notfound', {title: "Pagina no encontrada | Tecnosalud"})});
--- routes/profesional.js
+++ routes/profesional.js
@@ -1,9 +1,9 @@
 const routes = require('express').Router();
 
 //index
-routes.get('/', (req, res) => {res.render('index', {title: 'Dashboard de Profesional | Tecnosalud'})})
+//routes.get('/', (req, res) => {res.render('index', {title: 'Dashboard de Profesional | Tecnosalud'})})
 
-//profesional
-routes.get('/', (req, res) => {res.render('profesional/postprofesional', {title: 'Registrar Profesional | Tecnosalud', scripts: ["profesional/post.js"]})})
+//profesional/registrar
+routes.get('/registrar', (req, res) => {res.render('profesional/registrar', {title: 'Registrar Profesional | Tecnosalud', scripts: ["profesional/post.js"]})})
 
 module.exports = routes;
