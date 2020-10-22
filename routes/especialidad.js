06beb0206d05657d7c2bfcb8736b655b0ce61bab
julinuzzo19 <julinuzzo19@gmail.com>
2020-10-22 18:15:36 -0300

    routes/especialidad added

--- routes/especialidad.js
+++ routes/especialidad.js
@@ -4,4 +4,5 @@ const routes = require('express').Router();
 //especialidad/registrar
 routes.get('/registrar', (req, res) => {res.render('especialidad/registrar', {title: 'Registrar Especialidad | Tecnosalud', scripts: ["especialidad/post.js"]})})
 
+
 module.exports = routes;
