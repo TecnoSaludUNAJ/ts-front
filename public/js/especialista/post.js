8444cb031dba575b2e5385b906c1dba7e30e3bec
julinuzzo19 <julinuzzo19@gmail.com>
2020-10-28 00:28:33 -0300

    fixed especialista.js

--- public/js/especialista/post.js
+++ public/js/especialista/post.js
@@ -8,34 +8,27 @@ class Especialista
     }
 }
 
-
 function PostEspecialista(especialistajson)
 {
-
     fetch('https://localhost:44306/api/Especialistas',{
     method: 'POST',
     body: especialistajson,
     headers:{'Content-Type': 'application/json;charset=UTF-8'}
-    }).then(response =>response.json()).then(data=>{console.log(data);})
-
+    }).then(response =>response.json())
 
 }
-
 
 const dom= document.getElementById("form-especialista").addEventListener("submit",function(event){
 
     event.preventDefault();
     
-    var especialidadId=document.getElementById("input-especialista-especialidadid").value;
-    var profesionalId=document.getElementById("input-especialista-profesionalid").value;
-    var calendarioTurnos=document.getElementById("input-especialista-calendarioturnosid").value;
+    var especialidadId=Number.parseInt(document.getElementById("input-especialista-especialidadid").value);
+    var profesionalId=Number.parseInt(document.getElementById("input-especialista-profesionalid").value);
+    var calendarioTurnos=Number.parseInt(document.getElementById("input-especialista-calendarioturnosid").value);
 
-
-   
     var especialista= new Especialista(profesionalId,especialidadId,calendarioTurnos);
 
     var especialistajson=JSON.stringify(especialista);
-
 
     PostEspecialista(especialistajson);
 
