eeee7e7f07ebe1d2298c1dc2437990af9bca3267
julinuzzo19 <julinuzzo19@gmail.com>
2020-11-01 20:27:12 -0300

    registrar especialista con select de especialidades

--- public/js/especialista/post.js
+++ public/js/especialista/post.js
@@ -6,8 +6,6 @@ class Especialista {
   }
 }
 
-
-
 function PostEspecialista(especialistajson) {
   fetch("https://localhost:44306/api/Especialistas", {
     method: "POST",
@@ -28,7 +26,6 @@ function mostrarEspecialidades(lista) {
   for (let i of lista) {
     const place = document.getElementById("listespecialidades");
     const element = document.createElement("div");
-
     element.innerHTML = `
     <option value="${i.tipoEspecialidad}">
     `;
@@ -37,48 +34,43 @@ function mostrarEspecialidades(lista) {
 }
 
 window.onload = function () {
-  getEspecialidades(null);
+  getEspecialidades();
 };
 
-
-
-function getId(especialidad) {
+//Post de Especialista obteniendo el id de la especialidad seleccionada
+function getId(profesionalId, calendarioTurnos, especialidad) {
   fetch("https://localhost:44306/api/Especialidades")
     .then((response) => response.json())
     .then((lista) => {
       for (const item of lista) {
         if (especialidad == item.tipoEspecialidad) {
-          return item.id;
+          var especialidadId = item.id;
+
+          console.log(profesionalId, calendarioTurnos, especialidadId);
+
+          var especialista = new Especialista(
+            profesionalId,
+            especialidadId,
+            calendarioTurnos
+          );
+
+          var especialistajson = JSON.stringify(especialista);
+
+          PostEspecialista(especialistajson);
         }
       }
     });
 }
 
-const dom = document
-  .getElementById("form-especialista")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    var especialidad = document.getElementById(
-      "input-especialista-especialidadid"
-    ).value;
-
-    var especialidadId= getId(especialidad);
-
-    var profesionalId = Number.parseInt(
-      document.getElementById("input-especialista-profesionalid").value
+const formespecialista = document.getElementById("form-especialista");
+if (formespecialista) {
+  formespecialista.onsubmit = (e) => {
+    e.preventDefault();
+    var profesionalId = parseInt(formespecialista.elements.profesional.value);
+    var calendarioTurnos = parseInt(
+      formespecialista.elements.calendarioturnos.value
     );
-    var calendarioTurnos = Number.parseInt(
-      document.getElementById("input-especialista-calendarioturnosid").value
-    );
-
-    var especialista = new Especialista(
-      profesionalId,
-      especialidadId,
-      calendarioTurnos
-    );
-
-    var especialistajson = JSON.stringify(especialista);
-
-    PostEspecialista(especialistajson);
-  });
+    var especialidad = formespecialista.elements.especialidad.value;
+    getId(profesionalId, calendarioTurnos, especialidad);
+  };
+}
