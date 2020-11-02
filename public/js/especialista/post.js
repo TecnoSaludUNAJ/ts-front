683112257571b4a0034140e99efecf9be84f5ec9
julinuzzo19 <julinuzzo19@gmail.com>
2020-11-01 21:58:01 -0300

    syntax optimizations

--- public/js/especialista/post.js
+++ public/js/especialista/post.js
@@ -6,6 +6,8 @@ class Especialista {
   }
 }
 
+var profesionalId;
+
 function PostEspecialista(especialistajson) {
   fetch("https://localhost:44306/api/Especialistas", {
     method: "POST",
@@ -22,6 +24,43 @@ const getEspecialidades = function () {
     });
 };
 
+async function getProfesionales(
+  profesionalname,
+  calendarioTurnos,
+  especialidadId
+) {
+  await fetch("https://localhost:44306/api/Profesionales")
+    .then((response) => response.json())
+    .then((lista) => {
+      if (profesionalname == null || profesionalname == undefined) {
+        mostrarProfesionales(lista);
+      } else {
+        console.log("entre");
+        for (const item of lista) {
+          console.log(profesionalname, "==", `${item.nombre} ${item.apellido}`);
+
+          if (profesionalname == `${item.nombre} ${item.apellido}`) {
+            console.log("entreif");
+            profesionalId = item.id;
+            console.log(profesionalId);
+
+            var especialista = new Especialista(
+              profesionalId,
+              especialidadId,
+              calendarioTurnos
+            );
+
+            var especialistajson = JSON.stringify(especialista);
+
+            PostEspecialista(especialistajson);
+
+            break;
+          }
+        }
+      }
+    });
+}
+
 function mostrarEspecialidades(lista) {
   for (let i of lista) {
     const place = document.getElementById("listespecialidades");
@@ -33,44 +72,46 @@ function mostrarEspecialidades(lista) {
   }
 }
 
+function mostrarProfesionales(lista) {
+  for (let i of lista) {
+    const place = document.getElementById("listprofesionales");
+    const element = document.createElement("div");
+    element.innerHTML = `
+    <option value="${i.nombre} ${i.apellido}">
+    `;
+    place.appendChild(element);
+  }
+}
+
 window.onload = function () {
   getEspecialidades();
+  getProfesionales(null, null, null);
 };
 
-//Post de Especialista obteniendo el id de la especialidad seleccionada
-function getId(profesionalId, calendarioTurnos, especialidad) {
-  fetch("https://localhost:44306/api/Especialidades")
+//Obteniendo el id de la especialidad seleccionada y pasando los datos a getProsionales para obtener su Id
+async function getId(profesionalname, calendarioTurnos, especialidad) {
+  await fetch("https://localhost:44306/api/Especialidades")
     .then((response) => response.json())
     .then((lista) => {
       for (const item of lista) {
         if (especialidad == item.tipoEspecialidad) {
           var especialidadId = item.id;
 
-          console.log(profesionalId, calendarioTurnos, especialidadId);
-
-          var especialista = new Especialista(
-            profesionalId,
-            especialidadId,
-            calendarioTurnos
-          );
-
-          var especialistajson = JSON.stringify(especialista);
-
-          PostEspecialista(especialistajson);
+          getProfesionales(profesionalname, calendarioTurnos, especialidadId);
         }
       }
     });
 }
-
+//Obteniendo datos del form
 const formespecialista = document.getElementById("form-especialista");
 if (formespecialista) {
   formespecialista.onsubmit = (e) => {
     e.preventDefault();
-    var profesionalId = parseInt(formespecialista.elements.profesional.value);
+    var profesionalname = formespecialista.elements.profesional.value;
     var calendarioTurnos = parseInt(
       formespecialista.elements.calendarioturnos.value
     );
     var especialidad = formespecialista.elements.especialidad.value;
-    getId(profesionalId, calendarioTurnos, especialidad);
+    getId(profesionalname, calendarioTurnos, especialidad);
   };
 }
