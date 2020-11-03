1bd98abf54a24c77c041e911f0b964280659c12d
Brian Alejandro Claros Maldonado <brianclarosmaldonado@gmail.com>
2020-11-03 20:36:10 -0300

    Added days to constant for index of getDay

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://localhost:44377/api";
 export const TURNOS_API_URL = API_URL + "/turnos";
 export const TURNOS_DISPONIBLES_API_URL = API_URL + "/turnosdisponibles";
+export const DAYS = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
