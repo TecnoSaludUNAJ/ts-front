22c70f718a7828bc25d172619a6b236c3a401883
julinuzzo19 <julinuzzo19@gmail.com>
2020-11-10 22:33:01 -0300

    constants URL fixed

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,11 +1,24 @@
-export const API_URL = "https://localhost:44377/api";
+
+/* API TURNOS */
+export const API_URL = "https://localhost:44307/api";
 export const TURNOS_API_URL = API_URL + "/turnos";
 export const TURNOS_DISPONIBLES_API_URL = API_URL + "/turnosdisponibles";
 export const DAYS = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-export const ESPECIALIDADES_API_URL = "https://localhost:44306/api/Especialidades";
-export const ESPECIALISTAS_API_URL = "https://localhost:44306/api/Especialistas";
-export const PROFESIONALES_API_URL = "https://localhost:44306/api/Profesionales";
 
+export class CalendarioTurnos {
+  constructor(IdEspecialista,diaId,horaInicio,horaFin) {
+    this.idEspecialista=IdEspecialista,
+    this.diaId=diaId,
+    this.horaInicio=horaInicio,
+    this.horaFin=horaFin
+  }
+}
+
+/* API HOSPITAL */
+export const API_URL_HOSPITAL = "https://localhost:44308/api"
+export const ESPECIALIDADES_API_URL = API_URL_HOSPITAL +"/Especialidades";
+export const ESPECIALISTAS_API_URL = API_URL_HOSPITAL +"/Especialistas";
+export const PROFESIONALES_API_URL = API_URL_HOSPITAL +"/Profesionales";
 
 export class Especialista {
     constructor(profesionalid, especialidadid) {
@@ -20,24 +33,22 @@ export class Especialista {
     }
   }
 
-  export class CalendarioTurnos {
-    constructor(IdEspecialista,diaId,horaInicio,horaFin) {
-      this.idEspecialista=IdEspecialista,
-      this.diaId=diaId,
-      this.horaInicio=horaInicio,
-      this.horaFin=horaFin
-    }
-  }
-
 // API PACIENTES
-export const PACIENTES_API_URL = "https://localhost:5001/api/Pacientes"
-export const OBRASSOCIALES_API_URL = "https://localhost:5001/api/ObrasSociales"
+/* export const PACIENTES_API_URL = "https://localhost:5001/api/Pacientes"
+export const OBRASSOCIALES_API_URL = "https://localhost:5001/api/ObrasSociales" */
+export const PACIENTES_API_URL = "https://localhost:44310/api/Pacientes"
+export const OBRASSOCIALES_API_URL = "https://localhost:44310/api/ObrasSociales"
 
 //API HISTORIA CLINICA
-export const REGISTROS_API_URL = "https://localhost:5001/api/Registros"
+/* export const REGISTROS_API_URL = "https://localhost:5001/api/Registros"
 export const RECETAS_API_URL = "https://localhost:5001/api/Recetas"
 export const HISTORIA_API_URL = "https://localhost:5001/api/HistoriaClinicas"
-export const ANALISIS_API_URL = "https://localhost:5001/api/Analisis"
+export const ANALISIS_API_URL = "https://localhost:5001/api/Analisis" */
+export const REGISTROS_API_URL = "https://localhost:44306/api/Registros"
+export const RECETAS_API_URL = "https://localhost:44306/api/Recetas"
+export const HISTORIA_API_URL = "https://localhost:44306/api/HistoriaClinicas"
+export const ANALISIS_API_URL = "https://localhost:44306/api/Analisis"
+
 
 export class Registros{
   constructor(motivoConsulta,diagnostico,proximaRevision,especialistaId,historiaClinicaId){
