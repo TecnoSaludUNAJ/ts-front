36b0e1e60f5fbc85dc6f2a8011ac37f4d461e15a
julinuzzo19 <julinuzzo19@gmail.com>
2020-11-25 01:26:57 -0300

    added show registros paciente

---
+++ public/js/historiaclinica/historia.js
@@ -0,0 +1,50 @@
+import { HISTORIA_API_URL } from "../constants.js";
+
+function getHistoriaClinica(idpaciente) {
+  fetch(HISTORIA_API_URL + `?pacienteid=${idpaciente}`)
+  .then((res) =>res.json())
+  .then(data => {mostrarDatos(data)})
+  .catch(err=>console.log(err))
+}
+
+//Getting paciente ID
+const querystring = window.location.search;
+const urlparams = new URLSearchParams(querystring);
+const pacienteid = urlparams.get("pacienteid");
+
+getHistoriaClinica(pacienteid);
+
+function mostrarDatos(historialclinico) {
+
+  const container=document.getElementById("containerhistoria");
+  historialclinico.forEach(historial => {
+   
+    container.innerHTML+=`
+    <div class='card mb-3 border-primary'>
+        <div class="card-body">
+        <span id="motivo" class="  d-block border-bottom">Motivo de la consulta:  ${historial.motivoConsulta}
+            
+        </span><br>
+
+        <span id="diagnostico" class='  d-block border-bottom'>Diagnostico: ${historial.diagnostico}
+            
+        </span><br>
+
+        <span id="analisis" class='d-block border-bottom'>Detalle de los analisis: ${historial.descripcionAnalisis}
+            
+        </span><br>
+
+        <span id="receta" class='d-block border-bottom'>Detalle de recetas: ${historial.descripcionReceta}
+            
+        </span><br>
+
+        <span id="proximarevision" class='  d-block border-bottom'>Proxima revision: ${historial.proximaRevision}
+            
+        </span><br>
+        
+        </div>
+    </div>`
+
+  });
+
+}
--- public/js/historiaclinica/registros.js
+++ public/js/historiaclinica/registros.js
@@ -3,28 +3,48 @@ import { PACIENTES_API_URL } from "../constants.js";
 const formpacienteid = document.getElementById("form-input-paciente");
 
 window.addEventListener("load", () => {
-  formpacienteid.onsubmit = async function (e) {
-    e.preventDefault();
-    const pacientedni = document.getElementById("input-paciente").value;
-    await getPacienteByDni(pacientedni);
-  };
+  if (formpacienteid) {
+    formpacienteid.onsubmit = async function (e) {
+      e.preventDefault();
+      const pacientedni = document.getElementById("input-paciente").value;
+      await getPacienteByDni(pacientedni);
+    };
+  }
 });
 
 async function getPacienteByDni(pacientedni) {
   await fetch(PACIENTES_API_URL + `/dni/${pacientedni}`)
     .then((res) => res.json())
     .then((data) => {
-         mostrarDatos(data);
+      mostrarDatos(data);
     })
-    .catch(err=>console.log(err));
+    .catch((err) => console.log(err));
 }
 
 function mostrarDatos(paciente) {
-    console.log(paciente);
-    const nombre= document.querySelector("#nombre");
-    nombre.innerHTML=`Nombre: ${paciente.nombre}`;
-    const apellido= document.querySelector("#apellido");
-    apellido.innerHTML=`Apellido: ${paciente.apellido}`;
-    const dni= document.querySelector("#dni");
-    dni.innerHTML=`Numero de documento: ${paciente.dni}`;
+  const nombre = document.querySelector("#nombre");
+  const apellido = document.querySelector("#apellido");
+  const dni = document.querySelector("#dni");
+  const labelnotfound = document.getElementById("label-notfound");
+  const btnhistorial= document.getElementById("mostrarHistorial");
+
+
+  if (paciente.nombre !== undefined) {
+    labelnotfound.setAttribute("hidden", true);
+    nombre.innerHTML = `Nombre: ${paciente.nombre}`;
+    apellido.innerHTML = `Apellido: ${paciente.apellido}`;
+    dni.innerHTML = `Numero de documento: ${paciente.dni}`;
+    btnhistorial.setAttribute("href","/historiaClinica/historia"+`?pacienteid=${paciente.paciente_Id}`);
+
+ 
+  } else {
+    if (labelnotfound.hidden) {
+      labelnotfound.removeAttribute("hidden");
+      nombre.innerHTML = `Nombre: No se ha encontrado`;
+      apellido.innerHTML = `Apellido: No se ha encontrado`;
+      dni.innerHTML = `Numero de documento: No se ha encontrado`;
+    } else {
+      labelnotfound.setAttribute("hidden");
+    }
+  }
 }
